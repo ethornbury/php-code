b7751ce66755bc271f60f4dde6c5c2ef2ac79dea
ethornbury <thornburye@gmail.com>
2023-08-09 09:54:56 +0100

    starting update functionality

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Pizza & Pasta</title>
+		<title>Read functionality</title>
 		<meta charset="utf-8">
 		  <meta name="viewport" content="width=device-width, initial-scale=1">
 		  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -29,7 +29,7 @@
                     <a class="nav-link" href="booking.html">Book</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="online.html">Online</a>
+                    <a class="nav-link" href="update-booking.php">Update booking</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="search.html">Search</a>
---
+++ update-booking.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Update functionality</title>
+		<meta charset="utf-8">
+		  <meta name="viewport" content="width=device-width, initial-scale=1">
+		  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+		  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="https://kit.fontawesome.com/096239daca.js" crossorigin="anonymous"></script>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Fredericka+the+Great&family=Montserrat:wght@300&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="mystyle.css">
+		 <link rel="stylesheet" href="path/to/icon-library.css">
+	</head>
+	<body class="container-fluid">
+		<nav class="navbar navbar-expand-sm bg-light">
+
+		  <div class="container-fluid">
+			<!-- Links -->
+			<ul class="navbar-nav">
+			  <li class="nav-item">
+				  <a class="nav-link" href="index.html">Home</a>
+			  </li>
+			  <li class="nav-item">
+				  <a class="nav-link" href="menu.html">Menu</a>
+			  </li>
+			  <li class="nav-item">
+				  <a class="nav-link" href="booking.html">Book</a>
+			  </li>
+			  <li class="nav-item">
+            <a class="nav-link" href="update-booking.php">Update booking</a>
+        </li>
+			  <li class="nav-item">
+          <a class="nav-link" href="search.php">Search</a>
+			  </li>
+			</ul>
+		  </div>
+
+		</nav>
+    <div class="row">
+      <div class="col-md-2"></div>
+      <div class="col-md-8">
+        <h2>UPDATE YOUR BOOKING <i class="fas fa-utensils fa-sm"></i></h2> 
+        <br>
+        <form action="update-booking.php" method="post">
+            <div class="row">
+                <div class="col">
+                <select name="book_day">
+                    <option value="day-select" class="form-select"  >Select Day</option>
+                    <option value="wed">Wednesday</option>
+                    <option value="thur">Thursday</option>
+                    <option value="fri">Friday</option>
+                    <option value="sat">Saturday</option>
+                    <option value="sun">Sunday</option>
+                </select>
+                </div>
+                <div class="col">
+                <select name="book_time">
+                    <option value="hour-select">Select time</option>
+                    <option value="15:00">15:00</option>
+                    <option value="15:15">15:15</option>
+                    <option value="15:30">15:30</option>
+                    <option value="15:45">15:45</option>
+                    <option value="16:00">16:00</option>
+                    <option value="16:15">16:15</option>
+                    <option value="16:30">16:30</option>
+                    <option value="16:45">16:45</option>
+                    <option value="17:00">17:00</option>
+                    
+                </select>
+                </div>
+
+            </div>
+            <br>
+
+            <div class="mb-3">
+                <label class="form-label">Name:</label>
+                <input type="text" class="form-control" placeholder="Enter booking full name" name="book_name">
+            </div>
+            <br>
+
+            <button type="submit" class="btn btn-info">SEARCH</button>
+        </form>
+			
+			
+<!-- php sitting here to pick up the column div -->
+<?php
+//set up
+$servername = "localhost"; //server name
+$username = "root"; //MySQL username
+$password = ""; //MySQL password
+$dbname = "booking_db"; //your database name
+
+// Create a new MySQLi instance under your var name $mycon
+$mycon = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($mycon -> connect_error) {
+    die("Connection failed: " . $mycon -> connect_error);
+} else {
+   //echo "Connected successfully"; //testing line
+
+    if (isset($_POST['book_day'], $_POST["book_time"], $_POST['book_name'])) {
+        //getting content from html form -> save to php var -> pass to mysql query 
+        //var_dump($_POST["book_day"], $_POST["book_time"],$_POST["book_name"]); //html fields
+				
+        $days = htmlspecialchars($_POST["book_day"]);
+        $hours = htmlspecialchars($_POST["book_time"]);
+        $booking_name = htmlspecialchars($_POST["book_name"]);
+        // var_dump($days, $booking_name);
+        $sql = "SELECT * FROM booking"; //get all
+        //get a specific search
+        //$sql = "SELECT * FROM booking WHERE book_day LIKE ( '  ".$days." ') AND ( '  ".$booking_name." ')    ";
+        $result = $mycon->query($sql);
+		
+		
+		
+        var_dump($result);
+        if ($result -> num_rows > 0) {
+            // output data of each row
+            echo '<br><br> <ul class="list-group"> ';
+            while($row = $result -> fetch_assoc()) {
+                echo '<li class="list-group-item"> day: '. $row["book_day"]. ' | </i> time: '. $row["book_time"]. ' | name: ' . $row["book_name"] .'</li>';  
+            }  
+
+            echo '</ul>';
+        } else {
+            //echo "0 results";
+
+            echo '
+                <br><br>
+                <ul class="list-group">   
+                    <li class="list-group-item">no results to display</li> 
+                </ul>
+            
+            ';
+        }
+    }
+    // Close the connection
+    $mycon->close();
+}
+ //echo "<p>working</p>"; //testing line to check file is being picked up
+?>
+
+        </div> <!-- end of middle column with form -->
+
+        <div class="col-md-2"></div>
+        
+		
+	</div><!-- end row -->
+	<br><br>
+	<div class="row">
+		<div class="col-md-2"></div>
+        <div class="col-md-8">
+
+		  <form action="update-booking.php" method="post">
+          <div class="row">
+            <div class="col">
+              <select name="book_day">
+                <option value="day-select" class="form-select"  >Change Date</option>
+                <option value="wed">Wednesday</option>
+                <option value="thur">Thursday</option>
+                <option value="fri">Friday</option>
+                <option value="sat">Saturday</option>
+                <option value="sun">Sunday</option>
+              </select>
+            </div>
+            <div class="col">
+              <select name="book_time">	
+                <option value="hour-select">Change Hour</option>
+                <option value="15:00">15:00</option>
+                <option value="15:15">15:15</option>
+                <option value="15:30">15:30</option>
+                <option value="15:45">15:45</option>
+                <option value="16:00">16:00</option>
+                <option value="16:15">16:15</option>
+                <option value="16:30">16:30</option>
+                <option value="16:45">16:45</option>
+                <option value="17:00">17:00</option>
+               
+              </select>
+            </div>
+
+          </div>
+          <br>
+
+          <div class="mb-3">
+            <label class="form-label">Name:</label>
+            <input type="text" class="form-control" id="pwd" placeholder="Enter full name" name="book_name" disabled>
+          </div>
+          <br>
+
+          <div class="mb-3">
+            <label class="form-label">Number of people:</label>
+            <input type="number" class="form-control" placeholder="Update number" name="book_num" min="1">
+          </div>
+
+          <button type="submit" class="btn btn-primary">UPDATE</button>
+        </form>
+      </div>
+      <div class="col-md-2"></div>
+    </div>
+
+</body>
+</html>
